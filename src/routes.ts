3894dd11a297a6d6e4ffdf352f8f9eba4d72d09e
guilhermekonell <gui.ri.konell@hotmail.com>
2020-05-06 16:39:51 -0300

    Definindo tipagem com interface

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export default function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'guilherme@teste.com',
+    password: '123456',
+    techs: [
+      'NodeJs',
+      'React', 
+      'React Native',
+      { title: 'Javascript', experience: 100 }
+    ]
+  });
+
   return response.json({ message: 'Hello World' });
 }
